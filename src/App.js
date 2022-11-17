a4a5dd377560a650dbea241c7ef14fb95ba85131
Abai <abai.kuseinov.kg@gmail.com>
2022-11-17 19:48:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import useCounter from './hooks/useCounter'
+let number = 9
+function App() {
+  const {changeNumberInc, changeNumberDec,count,resetNumber } = useCounter(number,5,50,0)
+  
+  return (
+    <div>
+      {/* <input value={count} type="text" /> */}
+      <h1>{count}</h1>
+      <button onClick={changeNumberInc}>+</button>
+      <button onClick={changeNumberDec}>-</button>
+      <button onClick={resetNumber}>reset</button>
+    </div>
+  )
+}
+
+export default App
